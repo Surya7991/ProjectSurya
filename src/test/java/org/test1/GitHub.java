d81cf47908c576fad3af60b9f236db099061388c
suryAyrusSurya <ayrus564@gmail.com>
2022-05-26 22:04:20 +0530

    firstcommit

--- src/test/java/org/test1/GitHub.java
+++ src/test/java/org/test1/GitHub.java
@@ -1,5 +1,17 @@
 package org.test1;
 
 public class GitHub {
+	
+	private void m1() {
+		System.out.println("M1");
 
+	}
+	private void m2() {
+System.out.println("M2");
+	}
+
+	private void m3() {
+		System.out.println("M3");
+
+	}
 }
