b567231e756af43a6174e42ec8ebbc5368da2185
suryAyrusSurya <ayrus564@gmail.com>
2022-05-27 15:40:12 +0530

    method allign

--- src/test/java/org/test1/GitHub.java
+++ src/test/java/org/test1/GitHub.java
@@ -1,13 +1,13 @@
 package org.test1;
 
 public class GitHub {
-	
+
 	private void m1() {
 		System.out.println("M1");
 
 	}
 	private void m2() {
-System.out.println("M2");
+		System.out.println("M2");
 	}
 
 	private void m3() {
